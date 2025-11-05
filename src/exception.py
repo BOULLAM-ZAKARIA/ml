efa988503f42e2561482e97afe35fe5b2d5088e5
Zakaria Boullam <Zakaria.BOULLAM@emines.um6p.ma>
2025-11-05 19:22:43 +0100

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -10,7 +10,7 @@ def error_message_detail(error,error_detail:sys):
     )
     return error_message
 
-class CusromException(Exception):
+class CustomException(Exception):
     def __init__(self, error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
@@ -18,10 +18,10 @@ class CusromException(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info('Divide by zero')
-        raise CusromException(e,sys)
+# if __name__=="__main__":
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info('Divide by zero')
+#         raise CusromException(e,sys)
     
